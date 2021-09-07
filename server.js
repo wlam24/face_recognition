adad3e5f9cad1c3a15a8ca2580713f83384e8880
Winston Lam <winston.lam@Winstons-MacBook-Pro.local>
2021-09-06 20:37:40 -0400

    fix

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ import HandleApiCall  from './controllers/HandleApiCall.js';
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'process.env.DATABASE_URL',
+    connectionString: 'process.env.DATABASE_URL',
     ssl: true,
   }
 });
